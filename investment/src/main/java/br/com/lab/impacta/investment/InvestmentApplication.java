317560c94ab2e7b1e9f9dfe4e49bd7bbb3e39a3c
Rafael Nicoleti <rafael.nicoleti@faculdadeimpacta.com.br>
2022-10-04 19:08:21 -0300

    First Commit Investimentos

---
+++ investment/src/main/java/br/com/lab/impacta/investment/InvestmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.lab.impacta.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentApplication.class, args);
+	}
+
+}
---
+++ investment/src/test/java/br/com/lab/impacta/investment/InvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lab.impacta.investment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
